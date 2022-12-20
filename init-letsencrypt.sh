352d7d1dc9fcdcefa6dbfc9fc12c99d047d73e71
PSI <tkddls8848@naver.com>
2022-12-20 21:32:54 +0900

    move certification file directory

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains="naraapi.com"
 rsa_key_size=4096
-data_path="./certbot"
+data_path="/certbot"
 email="tkddls8848@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
