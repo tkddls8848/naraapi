a2a8a8d0cff491a9c96b22265c656c0676768b09
PSI <tkddls8848@naver.com>
2022-12-20 23:10:09 +0900

    split product enviroment and develope enviroment

--- frontend/httpsserver.js
+++ frontend/httpsserver.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const next = require("next")
 const mongoose = require('mongoose')
-require('dotenv').config({ path: './config_files/.env.https.local' })
+require('dotenv').config({ path: './config_files/.env.https.product' })
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const dev = process.env.NODE_ENV !== "production"
